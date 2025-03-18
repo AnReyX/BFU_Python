4ebb118784d62c486f3f88a14555c52a93bee92f
AndrewG <151661194+AnReyX@users.noreply.github.com>
2025-03-18 21:24:58 +0200

    Finished lab6.py

--- lab6.py
+++ lab6.py
@@ -16,7 +16,7 @@ try:
     if len(name) != 2:
         name = input("Input file name: ").strip(".json")
     else:
-        name = sys.argv[1]
+        name = sys.argv[1].strip(".json")
     with open(f"{name}.json") as json_file:
         data = json.load(json_file)
     with open(f"{name}.csv", "w") as csv_file:
